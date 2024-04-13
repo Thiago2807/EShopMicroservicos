c187a0f9e1c76d87950fba65b139e6af5107efd0
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-13 19:11:44 -0300

    Configurando interfaces genericas para o MediatR.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TQuery> : IQueryHandler<TQuery, Unit> where TQuery : IQuery<Unit>
+{
+
+}
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TResponse : notnull where TQuery : IQuery<TResponse>
+{
+
+}
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,27 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var product = new Product()
+        {
+            Name = command.Name,
+            Price = command.Price,
+            Category = command.Category,
+            ImageFile = command.ImageFile,
+            Description = command.Description,
+        };
+
+        // Salvar no banco de dados
+
+        return new CreateProductResult(Guid.Empty);
     }
 }
+ 
