6ae85434d02ef04b9b55a2ea16dc4398ccc58f90
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-13 20:10:30 -0300

    Registrando os serviços.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Adicionar serviços de um container.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 // Configuração das requisições HTTP pipeline.
+app.MapCarter();
 
 app.Run();
