363fb2969ee60adbb318a6c563cd568b94d70bad
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-22 20:25:39 -0300

    Criando endpoint para deletar um determinado produto.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.UptadeProduct;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeletarProduto")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Deletar produto")
+        .WithDescription("Realizar a remoção de um determinado produto.");
+    }
+}
