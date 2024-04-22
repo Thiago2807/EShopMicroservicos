62f58ebd0a437d4ea76b3191360299391f9b9090
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-22 17:29:57 -0300

    Criando endpoint para obter produtor em geral.

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
@@ -1,5 +1,26 @@
-﻿namespace Catalog.API.Products.GetProduct;
+﻿
+using Catalog.API.Products.CreateProduct;
 
-public class GetProductEndpoint
+namespace Catalog.API.Products.GetProduct;
+
+public record GetProductResponse(IEnumerable<Product> Products);
+
+public class GetProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = result.Adapt<GetProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("ObterProdutos")
+        .Produces<GetProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Obter Produtos")
+        .WithDescription("Obter os detalhes dos produtos em geral.");
+    }
 }
