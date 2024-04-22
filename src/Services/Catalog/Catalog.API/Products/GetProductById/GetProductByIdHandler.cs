bbde32a8d76a895a740ed89efa1720325f55dfaf
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-22 18:14:00 -0300

    Criando um manipulador para obter os produtos por um determinado Id.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductByIdHandler> logger) 
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdHandler.Handle called with {@Query}", query);
+
+        var result = await session.LoadAsync<Product>(query.Id, cancellationToken) ?? throw new ProductNotFoundException();
+
+        return new GetProductByIdResult(result);
+    }
+}
