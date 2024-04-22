0e83997bc018a70ba9d6a6a6724ed9eeedd344db
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-22 18:42:42 -0300

    Criando endpoint para obter os produtos por categoria.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", 
+            async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+
+            var products = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(products);
+        })
+        .WithName("ObterPorCategoriaProdutos")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Obter Por Categoria Produtos")
+        .WithDescription("Obter os produtos por categoria.");
+    }
+}
