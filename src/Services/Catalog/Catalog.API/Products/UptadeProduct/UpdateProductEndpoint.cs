b4bbf0a378a50c8c6d3f23267078d1c5a3a4452f
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-22 20:05:45 -0300

    Criando um endpoint para realizar a atualização de um determinado produto.

---
+++ src/Services/Catalog/Catalog.API/Products/UptadeProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.UptadeProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record UpdateProductResponse(bool IsSucess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest product, ISender sender) =>
+        {
+            var command = product.Adapt<UpdateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("AtualizarProduto")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Atualizar dados do produto")
+        .WithDescription("Realizar a atualização dos dados de um determinado produto.");
+    }
+}
