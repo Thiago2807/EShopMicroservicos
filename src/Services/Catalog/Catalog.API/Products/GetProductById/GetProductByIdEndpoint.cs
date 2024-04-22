2b27a30cdf65c738af64a3ff4298c787c3df63dd
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-22 18:14:26 -0300

    Criando um endpoint para obter um determinado produto por seu Id.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Products.GetProduct;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("ObterPorIdProdutos")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Obter Por Id Produtos")
+        .WithDescription("Obter os detalhe de um produto.");
+    }
+}
