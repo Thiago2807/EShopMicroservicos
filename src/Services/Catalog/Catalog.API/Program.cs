8550115715f54aafc0edfa28e8134901a2d9c865
Thiago2807 <thiagodasilva410@gmail.com>
2024-04-28 17:55:35 -0300

    Inicializando o banco de dados com dados para o trabalho.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Adicionar serviços de um container.
@@ -16,6 +18,9 @@ builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
