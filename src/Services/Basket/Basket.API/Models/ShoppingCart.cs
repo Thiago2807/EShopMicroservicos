1ec1b8890f418e92f0e0c9320b309fc5fd8e160e
Thiago2807 <thiagodasilva410@gmail.com>
2024-05-01 08:43:10 -0300

    Realizando a criação das classes modelos para o microserviço "Carrinho".

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public ShoppingCart() { }
+    public ShoppingCart(string userName) => UserName = userName;
+
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
