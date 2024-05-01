8d701ce5526157ff6235b83316964ff0ad957183
Thiago2807 <thiagodasilva410@gmail.com>
2024-05-01 09:02:55 -0300

    Realizando a imprementação inicial do método "GetBasketHandler".

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,17 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketHandler ()
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        return new GetBasketResult(new()
+        {
+            Items = [],
+            UserName = "thiagodasilva2807@gmail.com"
+        });
+    }
 }
