b52f5d65ddf7d526220cc95d048d403335f9817a
Thiago2807 <thiagodasilva410@gmail.com>
2024-05-01 10:30:47 -0300

    Configurando o MediatR.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCarter();
 
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviors<,>));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
