d0abac8e9b9b86234e0e138b1c276d004e30e748
Thiago2807 <thiagodasilva410@gmail.com>
2024-05-16 21:27:41 -0300

    Adicionando o AutoMigrations

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -7,8 +7,16 @@ public class DiscountContext(DbContextOptions<DiscountContext> options) : DbCont
 {
     public DbSet<Coupon> Coupons { get; set; } = default!;
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Coupon>().HasData(
+                new() { Id = 1, ProductName = "Iphone X", Description= "IPhone Discount", Amount = 150 },
+                new() { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
+
     }
+
 }
