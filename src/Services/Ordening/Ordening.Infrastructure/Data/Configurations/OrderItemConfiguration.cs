1f1ea2ce31c020379a402e58043666dce7f9927a
Thiago2807 <thiagodasilva410@gmail.com>
2024-06-22 16:23:26 -0300

    Configuranto entidade para o banco de dados.

---
+++ src/Services/Ordening/Ordening.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordening.Domain.Models;
+using Ordening.Domain.ValueObjects;
+
+namespace Ordening.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(pk => pk.Id);
+        builder.Property(o => o.Id).HasConversion(
+            orderItemId => orderItemId.Value, dbId => OrderItemId.Of(dbId));
+
+        builder.HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(o => o.ProductId);
+
+
+        builder.HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(o => o.OrderId);
+
+        builder.Property(c => c.Quantity).IsRequired();
+        builder.Property(c => c.Price).IsRequired();
+    }
+}
