ccd33d25b8b05295c9f6d7c8cfb6dd8579fe7303
Thiago2807 <thiagodasilva410@gmail.com>
2024-07-06 19:33:41 -0300

    Criando endpoint para excluir uma ordem.

--- src/Services/Ordening/Ordening.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordening/Ordening.API/Endpoints/DeleteOrder.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordening.API.Endpoints;
+﻿
+using Ordening.Application.Orders.Commands.DeleteOrder;
+using Ordening.Application.Orders.Commands.UpdateOrder;
 
-public class DeleteOrder
+namespace Ordening.API.Endpoints;
+
+public record DeleteOrderResponse(bool IsSucess);
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(Id));
+
+            var response = result.Adapt<DeleteOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOrder")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Order")
+        .WithDescription("Delete Order");
+    }
 }
