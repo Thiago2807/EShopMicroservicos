8f8ab8ed42f8dee35cba3335eaeca8755f49d403
Thiago2807 <thiagodasilva410@gmail.com>
2024-07-06 16:21:11 -0300

    Criando logica para excluir uma order.

---
+++ src/Services/Ordening/Ordening.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordening.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler (IApplicationDbContext context)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var order = await context.Orders.FindAsync([Orderid.Of(command.OrderId)], cancellationToken);
+
+        if (order is null)
+            throw new OrderNotFoundException(command.OrderId);
+
+        context.Orders.Remove(order);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);
+    }
+}
