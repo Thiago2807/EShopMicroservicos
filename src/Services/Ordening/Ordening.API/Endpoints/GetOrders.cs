05ba6ad1658559acb6024ef31e4116312b835367
Thiago2807 <thiagodasilva410@gmail.com>
2024-07-06 19:50:19 -0300

    Criando endpoint para obter as ordens.

--- src/Services/Ordening/Ordening.API/Endpoints/GetOrders.cs
+++ src/Services/Ordening/Ordening.API/Endpoints/GetOrders.cs
@@ -1,5 +1,28 @@
-﻿namespace Ordening.API.Endpoints;
+﻿using BuildingBlocks.Pagination;
+using Ordening.Application.Orders.Queries.GetIrderByCustomer;
+using Ordening.Application.Orders.Queries.GetOrders;
 
-public class GetOrders
+namespace Ordening.API.Endpoints;
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrders : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersQuery(request));
+
+            var response = result.Adapt<GetOrdersResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrders")
+        .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders")
+        .WithDescription("Get Orders");
+    }
 }
