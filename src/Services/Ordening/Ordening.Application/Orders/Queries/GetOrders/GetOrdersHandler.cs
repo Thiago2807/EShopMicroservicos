2d6c7994c5fc6160daae15f19ffef5b54795d7fb
Thiago2807 <thiagodasilva410@gmail.com>
2024-07-06 18:15:23 -0300

    Implementando paginação.

---
+++ src/Services/Ordening/Ordening.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Ordening.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler (IApplicationDbContext context)
+    : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var totalCount = await context.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await context.Orders
+            .Include(o => o.OrderItems)
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(
+            new BuildingBlocks.Pagination.PaginatedResult<OrderDto>(
+                    pageIndex,
+                    pageSize,
+                    totalCount,
+                    orders.ToOrderDtoList()
+                ));
+    }
+}
