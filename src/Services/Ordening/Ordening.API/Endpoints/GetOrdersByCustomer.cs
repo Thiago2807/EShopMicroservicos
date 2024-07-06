c43a8d67fd5aed11d3497bd3488e92521ff5882a
Thiago2807 <thiagodasilva410@gmail.com>
2024-07-06 19:46:49 -0300

    Criando endpoint para obter as ordens pelo nome.

--- src/Services/Ordening/Ordening.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordening/Ordening.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordening.API.Endpoints;
+﻿using Ordening.Application.Orders.Queries.GetIrderByCustomer;
+using Ordening.Application.Orders.Queries.GetOrdersByName;
 
-public class GetOrdersByCustomer
+namespace Ordening.API.Endpoints;
+
+public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByCustomer : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetIrderByCustomerQuery(customerId));
+
+            var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByCustomer")
+        .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders By Customer")
+        .WithDescription("Get Orders By Customer");
+    }
 }
--- src/Services/Ordening/Ordening.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordening/Ordening.API/Endpoints/GetOrdersByName.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordening.API.Endpoints;
+﻿using Ordening.Application.Orders.Commands.DeleteOrder;
+using Ordening.Application.Orders.Queries.GetOrdersByName;
 
-public class GetOrdersByName
+namespace Ordening.API.Endpoints;
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> OrderDtos);
+
+public class GetOrdersByName : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (string OrderName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(OrderName));
+
+            var response = result.Adapt<DeleteOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByName")
+        .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders By Name")
+        .WithDescription("Get Orders By Name");
+    }
 }
