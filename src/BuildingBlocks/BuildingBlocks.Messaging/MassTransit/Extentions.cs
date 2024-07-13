d888a670fa6c090c4464a9f2ba8028f3f25d7d4a
Thiago2807 <thiagodasilva410@gmail.com>
2024-07-13 17:01:48 -0300

    Configurando o RabbitMQ.

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -1,5 +1,32 @@
-﻿namespace BuildingBlocks.Messaging.MassTransit;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
-public class Extentions
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extentions
 {
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configurantion, Assembly? assembly = null)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configurantion["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configurantion["MessageBroker:UserName"]!);
+                    host.Password(configurantion["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
 }
